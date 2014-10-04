760385ea7e1c31f064e760f40f1de8e5dc330920
Travis <travisechapman@gmail.com>
2014-10-04 19:55:00 -0400

    Made this solution much easier to read(no weird recursive switch statements)

--- clock/clock.go
+++ clock/clock.go
@@ -1,26 +1,37 @@
 package clock
 
-import "strconv"
+import "fmt"
 
 type Clock struct {
-    hrs int
-    mins int
+    hrs, mins int
 }
 
 func New(hours, minutes int) Clock {
-    switch {
-    case minutes >= 60:
-        return New(hours + 1, minutes - 60)
-    case minutes <= -60:
-        return New(hours - 1, minutes + 60)
-    case hours < 0:
-        return New(hours + 24, minutes)
-    case hours > 23:
-        return New(hours - 24, minutes)
-    case minutes < 0:
-        return New(hours - 1, minutes + 60)
+    var hr, min int
+    time := hours * 60 + minutes
+    if time > 0 {
+
+        for time >= 1440 {
+            time = time - 1440
+        }
+        hr = time / 60
+        min = time % 60
+
+        return Clock{hrs: hr, mins: min}
+
+    } else if time < 0 {
+
+        for time < 0 {
+            time = time + 1440
+        }
+        hr = time / 60
+        min = time % 60
+
+        return Clock{hrs: hr, mins: min}
+
+    } else {
+        return Clock{hrs: 0, mins: 0}
     }
-    return Clock{hrs: hours, mins: minutes}
 }
 
 func (c Clock) Add(minutes int) Clock {
@@ -28,17 +39,5 @@ func (c Clock) Add(minutes int) Clock {
 }
 
 func (c Clock) String() string {
-    var hours, minutes string
-    if c.hrs >= 10 {
-        hours = strconv.Itoa(c.hrs)
-    } else {
-        hours = "0" + strconv.Itoa(c.hrs)
-    }
-    if c.mins >= 10 {
-        minutes = strconv.Itoa(c.mins)
-    } else {
-        minutes = "0" + strconv.Itoa(c.mins)
-    }
-    result := hours + ":" + minutes
-    return result
+    return fmt.Sprintf("%02d:%02d", c.hrs, c.mins)
 }
